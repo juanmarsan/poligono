3a6583eb0b61a899e6c336dd4d143b7903f2d07b
Salvador <salvasanzandres@gmail.com>
2018-02-12 23:34:35 +0100

    corregir 2

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {EmpresasService} from '../../../core/domain/services/empresasService';
+import {PoligonosService} from '../../../core/domain/services/poligonosService';
+import {Poligono} from '../../../core/domain/models/poligono';
 
 @Component({
   selector: 'location-directive',
@@ -11,18 +13,22 @@ export class LocationComponent {
   public counter: number = 0;
   public empresas: any;
   public ordenarTipo: any;
-  public ordNombre: string;
-  public ordCategoria: string;
-  public ordPoligono: string;
+  public poligonos: any;
+  public poligonoFilter: string;
+  public filtroEmpresasNombre: string;
 
-  constructor(private empresasService: EmpresasService) {
+  constructor(private empresasService: EmpresasService,
+              private poligonosService: PoligonosService) {
     this.empresasService.getEmpresas().subscribe(response => {
       this.empresas = response;
+    });
+
+    this.poligonosService.getPoligonos().subscribe(response => {
+      this.poligonos = response;
     });
   }
 
   filterName(event: any) {
-    console.log(event);
     this.ordenarTipo = event;
   }
 }
